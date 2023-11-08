c796acfcd717fba9baf0176dd2b11bb94edc36f9
rosf73 <alswlzhsclq@naver.com>
2023-11-08 17:28:47 +0900

    feat/#24: post CRUD 작업 정의

--- lib/data/db_helper.dart
+++ lib/data/db_helper.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -24,9 +26,63 @@ class DBHelper {
     return await openDatabase(
       path,
       version: 1,
-      onCreate: (db, version) async {
-        // TODO : Create Table
+      onCreate: (db, version) async { // 가장 첫 DB 생성 시 호출. 그 이후에는 onUpgrade or onDowngrade 호출.
+        await db.execute(
+          "CREATE TABLE POST("
+              "id INTEGER PRIMARY KEY,"
+              "lat DOUBLE,"
+              "lon DOUBLE,"
+              "address TEXT,"
+              "rating TEXT,"
+              "review TEXT,"
+              "pictures JSON DEFAULT('[]'))",
+        );
       },
+    );
+  }
+
+  Future<void> insertToPost(double lat, double lon, String address, double rating, String review, List<String> pictures) async {
+    final db = await database;
+    await db.insert(
+      'POST', // Table name
+      { // Data
+        'lat': lat,
+        'lon': lon,
+        'rating': rating,
+        'address': address,
+        'review': review,
+        'pictures': jsonEncode(pictures),
+      },
+      conflictAlgorithm: ConflictAlgorithm.replace, // Duplicated Data Strategy
+    );
+  }
+
+  Future<List<Map<String, dynamic>>> selectAllPost() async {
+    final db = await database;
+    return await db.query('POST');
+  }
+
+  Future<void> updatePost(int id, double rating, String address, String review, List<String> pictures) async {
+    final db = await database;
+    await db.update(
+      'POST',
+      {
+        'rating': rating,
+        'address': address,
+        'review': review,
+        'pictures': jsonEncode(pictures),
+      },
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<void> deletePost(int id) async {
+    final db = await database;
+    await db.delete(
+      'POST',
+      where: 'id = ?',
+      whereArgs: [id],
     );
   }
 }
